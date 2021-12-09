45da54dc0a06fb811bdca0f8e832a4ba1a671cfa
James Harton <james@harton.nz>
2021-12-09 15:12:40 +1300

    chore(deps): update dependency ex_doc to >= 0.26.0

--- mix.exs
+++ mix.exs
@@ -38,7 +38,7 @@ defmodule Lamina.Dashboard.MixProject do
   defp deps do
     [
       {:credo, "~> 1.6", only: ~w[dev test]a},
-      {:ex_doc, ">= 0.0.0", only: ~w[dev test]a},
+      {:ex_doc, ">= 0.26.0", only: ~w[dev test]a},
       {:git_ops, "~> 2.3", only: ~w[dev test]a, runtime: false},
       {:jason, "~> 1.0", only: ~w[dev test]a},
       {:lamina, "~> 0.4"},
